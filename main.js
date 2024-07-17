75219b97669296a8d935d3d4c519c70f6ec50bec
Denis Kirillov <ds.kirillov@vk.team>
2024-07-17 13:15:26 +0300

    oauth2_authorize_response listener

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-window.addEventListener("message", (data) =>
-  console.log("messageEvent >", data)
-);
+function proccess(code, device_id) {
+  if (!(code && device_id)) {
+    return;
+  }
+  const accessToken = VKID.Auth.exchangeCode(code, device_id);
+
+  accessToken
+    .then((data) => console.log("promise resolved >", data))
+    .catch((error) => console.log("promise failed >", error));
+
+  console.log({
+    code,
+    device_id,
+    accessToken,
+  });
+}
+
+window.addEventListener("message", (event) => {
+  console.log("messageEvent >", event);
+  if (event.data?.action?.startsWith("oauth2_authorize_response")) {
+    console.log("> oauth2_authorize_response event", event.data?.payload);
+    const code = event.data?.payload?.code;
+    const device_id = event.data?.payload.device_id;
+
+    proccess(code, device_id);
+  }
+});
 
 const VKID = window.VKIDSDK;
 
@@ -18,15 +42,4 @@ VKID.Config.init({
 const params = new URL(location).searchParams;
 const code = params.get("code");
 const device_id = params.get("device_id");
-
-const accessToken = VKID.Auth.exchangeCode(code, device_id);
-
-accessToken
-  .then((data) => console.log("promise resolved >", data))
-  .catch((error) => console.log("promise failed >", error));
-
-console.log({
-  code,
-  device_id,
-  accessToken,
-});
+proccess(code, device_id);

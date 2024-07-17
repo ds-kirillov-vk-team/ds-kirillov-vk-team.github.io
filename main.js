e3c4474a8aeb25ed5f271e5f058810659022eef3
Denis Kirillov <ds.kirillov@vk.team>
2024-07-17 10:59:19 +0300

    domain fucked

---
+++ main.js
@@ -0,0 +1,17 @@
+const VKID = window.VKIDSDK;
+
+const params = new URL(location).searchParams;
+const code = params.get("code");
+const device_id = params.get("device_id");
+
+const accessToken = VKID.Auth.exchangeCode(code, device_id);
+
+accessToken
+    .then((data) => console.log('promise resolved >', data))
+    .catch((error) => console.log('promise failed >', error))
+
+console.log({
+  code,
+  device_id,
+  accessToken,
+});

b276615fd593dbe7d5fe0d496d232feb2b6122d8
Denis Kirillov <ds.kirillov@vk.team>
2024-07-17 10:21:49 +0300

    2.0.0

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const device_id = params.get("device_id");
 
 const accessToken = VKID.Auth.exchangeCode(code, device_id);
 
+accessToken
+    .then((data) => console.log('promise resolved >', data))
+    .catch((error) => console.log('promise failed >', error))
+
 console.log({
   code,
   device_id,
